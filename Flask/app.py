40ca056502aab4f4f07abe5b3f7c824fee7dfca5
paal215 <p_hs@hotmail.com>
2024-10-18 22:24:47 +0200

    Flere endpoints i app.py + endret navn på neo4j folder pga. kollisjon med neo4j når den importeres

--- Flask/app.py
+++ Flask/app.py
@@ -1,75 +1,112 @@
-from flask import Flask, request  
-# from fil import alle funksjonene i return setningene under
+from flask import Flask, request
+from neo4j_folder.DB import create_car, read_cars, update_car, delete_car
+from neo4j_folder.DB import create_customer, read_customers, update_customer, delete_customer
+from neo4j_folder.DB import create_employee, read_employees, update_employee, delete_employee
+from neo4j_folder.DB import order_car, cancel_order_car, rent_car, return_car
 
 app = Flask(__name__)
 
-# Endpoints (CRUD: Create, Read, Update, Delete er funksjoner som skal være plassert
-# i database python filen). Endpointsene under håndterer hva som skjer med HTTP
-# requests, og de returnerer/kaller på CRUD funksjonene i den andre filen
+# --------------------------------------------------------------------------------------------------------------------------------------
+#             Endpoints -> Cars:
+
 @app.route("/cars", methods=["POST"])
 def create_car_route():
     data = request.get_json()
-    return create_car(data['id'],data['make'], data['model'], data['year'], data['location'], data['status'])
+    create_car(data["car_id"],data["make"], data["model"], data["year"], data["location"], data["status"])
+    return "Car was created", 201
 
 @app.route("/cars", methods=["GET"])
 def read_cars_route():
-    return read_cars()
+    return read_cars(), 200
 
 @app.route("/cars", methods=["PUT"])
 def update_car_route():
     data = request.get_json()
-    return update_car(data["id"], data["make"], data["model"], data["year"], data["location"], data["status"])
+    update_car(data["car_id"], data["make"], data["model"], data["year"], data["location"], data["status"])
+    return "Car was updated", 200
 
 @app.route("/cars", methods=["DELETE"])
 def delete_car_route():
     data = request.get_json()
-    delete_car(data["id"]) # er dette riktig id/nøkkel? TROR DET
-    return get_all_cars() # skal jeg returnere denne, eller bare utføre slettingen TROR vi kan velge mellom begge deler, men idk, vi kan spørre Olav. 
+    delete_car(data["car_id"])
+    return "Car deleted", 200
 
-# CRUD (Create, Read, Update, Delete) for "Customer"
+# --------------------------------------------------------------------------------------------------------------------------------------
+#             Endpoints -> Customers:
+
 @app.route("/customers", methods=["POST"])
 def create_customer_route():
     data = request.get_json()
-    return create_customer(data["customer_id"],data["name"], data["age"], data["address"])
+    create_customer(data["customer_id"],data["name"], data["age"], data["address"])
+    return "Customer was created", 201
 
 @app.route("/customers", methods=["GET"])
 def read_customers_route():
-    return read_customers()
+    return read_customers(), 200
 
 @app.route("/customers", methods=["PUT"])
 def update_customer_route():
     data = request.get_json()
-    return update_customer(data["customer_id"],data["name"], data["age"], data["address"])
+    update_customer(data["customer_id"],data["name"], data["age"], data["address"])
+    return "Customer was updated", 200
 
 @app.route("/customers", methods=["DELETE"])
 def delete_customer_route():
     data = request.get_json()
-    delete_customer(data["customer_id"]) # er dette riktig id/nøkkel? VI KAN lage en customer id, kan være to kunder med samme navn:)
-    return get_all_customers() # skal jeg returnere denne, eller bare utføre slettingen
+    delete_customer(data["customer_id"])
+    return "Customer was deleted", 200
 
-# CRUD (Create, Read, Update, Delete) for "Employees"
+# --------------------------------------------------------------------------------------------------------------------------------------
+#             Endpoints -> Employees:
+
 @app.route("/employees", methods=["POST"])
 def create_employee_route():
     data = request.get_json()
-    return create_employee(data["employee_id"],data["name"], data["address"], data["branch"])
+    create_employee(data["employee_id"],data["name"], data["address"], data["branch"])
+    return "Employee was created", 201
 
 @app.route("/employees", methods=["GET"])
 def read_employees_route():
-    return read_employees()
+    return read_employees(), 200
 
 @app.route("/employees", methods=["PUT"])
 def update_employee_route():
     data = request.get_json()
-    return update_employee(data["employee_id"],data["name"], data["address"], data["branch"])
+    update_employee(data["employee_id"],data["name"], data["address"], data["branch"])
+    return "Employee was created", 200
 
 @app.route("/employees", methods=["DELETE"])
 def delete_employee_route():
     data = request.get_json()
-    delete_employee(data["name"]) # er dette riktig id/nøkkel? Vi legger inn employee id :)
-    return get_all_employees() # skal jeg returnere denne, eller bare utføre slettingen
+    delete_employee(data["employee_id"])
+    return "Employee was deleted", 200
 
-# Endpoints ("order-car", "cancel-order-car", "rent-car", "return-car")
+# --------------------------------------------------------------------------------------------------------------------------------------
+#             Endpoints -> order-car, cancel-order-car, rent-car, return-car:
+
 @app.route("/order-car", methods=["POST"])
+def order_car_route():
+    data = request.get_json()
+    var = order_car(data["customer_id"], data["car_id"])
+    return var, 200
+
+@app.route("/cancel-order-car", methods=["POST"])
+def cancel_order_car_route():
+    data = request.get_json()
+    var = cancel_order_car(data["customer_id"], data["car_id"])
+    return var, 200
+
+@app.route("/rent-car", methods=["POST"])
+def rent_car_route():
+    data = request.get_json()
+    var = rent_car(data["customer_id"], data["car_id"])
+    return var, 200
+
+@app.route("/return-car", methods=["POST"])
+def return_car_route():
+    data = request.get_json()
+    var = return_car(data["customer_id"], data["car_id"], data["status"])
+    return var, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
