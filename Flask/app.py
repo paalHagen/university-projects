2563b56f901dd8093846d8da4720f1b8ec69c978
paal215 <p_hs@hotmail.com>
2024-10-24 12:05:42 +0200

    Forandret navn på neo4j folder, lagt inn from neo4j...,  lagt inn resten av endpointsene, forandret hva som returneres.

--- Flask/app.py
+++ Flask/app.py
@@ -6,33 +6,28 @@ from neo4j_folder.DB import order_car, cancel_order_car, rent_car, return_car
 
 app = Flask(__name__)
 
-# return i DB.py: enten boolean eller status code. Hvis boolean så kan jeg skrive if result i app.py,
-# hvis status code så kan jeg skrive if result == 201 i app.py.
-
 # --------------------------------------------------------------------------------------------------------------------------------------
 #             Endpoints -> Cars:
 
 @app.route("/cars", methods=["POST"])
 def create_car_route():
     data = request.get_json()
-    create_car(data["car_id"],data["make"], data["model"], data["year"], data["location"], data["status"])
-    return "Car was created", 201
+    return create_car(data["car_id"],data["make"], data["model"], data["year"], data["location"], data["status"])
 
 @app.route("/cars", methods=["GET"])
 def read_cars_route():
-    return read_cars(), 200
+    return read_cars()
 
 @app.route("/cars", methods=["PUT"])
 def update_car_route():
     data = request.get_json()
-    update_car(data["car_id"], data["make"], data["model"], data["year"], data["location"], data["status"])
-    return "Car was updated", 200
+    return update_car(data["car_id"], data["make"], data["model"], data["year"], data["location"], data["status"])
 
 @app.route("/cars", methods=["DELETE"])
 def delete_car_route():
     data = request.get_json()
     delete_car(data["car_id"])
-    return "Car deleted", 200
+    return read_cars()
 
 # --------------------------------------------------------------------------------------------------------------------------------------
 #             Endpoints -> Customers:
@@ -40,24 +35,22 @@ def delete_car_route():
 @app.route("/customers", methods=["POST"])
 def create_customer_route():
     data = request.get_json()
-    create_customer(data["customer_id"],data["name"], data["age"], data["address"])
-    return "Customer was created", 201
+    return create_customer(data["customer_id"],data["name"], data["age"], data["address"])
 
 @app.route("/customers", methods=["GET"])
 def read_customers_route():
-    return read_customers(), 200
+    return read_customers()
 
 @app.route("/customers", methods=["PUT"])
 def update_customer_route():
     data = request.get_json()
-    update_customer(data["customer_id"],data["name"], data["age"], data["address"])
-    return "Customer was updated", 200
+    return update_customer(data["customer_id"],data["name"], data["age"], data["address"])
 
 @app.route("/customers", methods=["DELETE"])
 def delete_customer_route():
     data = request.get_json()
     delete_customer(data["customer_id"])
-    return "Customer was deleted", 200
+    return read_customers()
 
 # --------------------------------------------------------------------------------------------------------------------------------------
 #             Endpoints -> Employees:
@@ -65,24 +58,22 @@ def delete_customer_route():
 @app.route("/employees", methods=["POST"])
 def create_employee_route():
     data = request.get_json()
-    create_employee(data["employee_id"],data["name"], data["address"], data["branch"])
-    return "Employee was created", 201
+    return create_employee(data["employee_id"],data["name"], data["address"], data["branch"])
 
 @app.route("/employees", methods=["GET"])
 def read_employees_route():
-    return read_employees(), 200
+    return read_employees()
 
 @app.route("/employees", methods=["PUT"])
 def update_employee_route():
     data = request.get_json()
-    update_employee(data["employee_id"],data["name"], data["address"], data["branch"])
-    return "Employee was created", 200
+    return update_employee(data["employee_id"],data["name"], data["address"], data["branch"])
 
 @app.route("/employees", methods=["DELETE"])
 def delete_employee_route():
     data = request.get_json()
     delete_employee(data["employee_id"])
-    return "Employee was deleted", 200
+    return read_employees()
 
 # --------------------------------------------------------------------------------------------------------------------------------------
 #             Endpoints -> order-car, cancel-order-car, rent-car, return-car:
@@ -90,26 +81,22 @@ def delete_employee_route():
 @app.route("/order-car", methods=["POST"])
 def order_car_route():
     data = request.get_json()
-    var = order_car(data["customer_id"], data["car_id"])
-    return var, 200
+    return order_car(data["customer_id"], data["car_id"])
 
 @app.route("/cancel-order-car", methods=["POST"])
 def cancel_order_car_route():
     data = request.get_json()
-    var = cancel_order_car(data["customer_id"], data["car_id"])
-    return var, 200
+    return cancel_order_car(data["customer_id"], data["car_id"])
 
 @app.route("/rent-car", methods=["POST"])
 def rent_car_route():
     data = request.get_json()
-    var = rent_car(data["customer_id"], data["car_id"])
-    return var, 200
+    return rent_car(data["customer_id"], data["car_id"])
 
 @app.route("/return-car", methods=["POST"])
 def return_car_route():
     data = request.get_json()
-    var = return_car(data["customer_id"], data["car_id"], data["status"])
-    return var, 200
+    return return_car(data["customer_id"], data["car_id"], data["status"])
 
 if __name__ == "__main__":
     app.run(debug=True)
